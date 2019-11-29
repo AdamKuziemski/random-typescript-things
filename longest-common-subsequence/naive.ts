3cabf1b911189b8b43139cc8cabc85bde1c142c3
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2019-11-30 00:16:10 +0100

    changed functions to one-liners

--- longest-common-subsequence/naive.ts
+++ longest-common-subsequence/naive.ts
@@ -1,20 +1,11 @@
-const longestSubsequence = (s1: string, s2: string): string => {
-  if (s1 === '' || s2 === '') {
-    return '';
-  }
+const longestSubsequence = (s1: string, s2: string): string => 
+  generateAllCommonSubsequences(s1, s2).sort((a, b) => b.length - a.length)[0] || '';
 
-  return generateAllCommonSubsequences(s1, s2).sort((a, b) => b.length - a.length)[0] || '';
-}
+const generateAllCommonSubsequences = (s1: string, s2: string): string[] =>
+  Array.from(s1).map((_, index) => commonSubsequence(s1.substr(index), s2)).filter(sub => sub !== '');
 
-const generateAllCommonSubsequences = (s1: string, s2: string): string[] => {
-  return Array.from(s1)
-    .map((_, index) => commonSubsequence(s1.substr(index), s2))
-    .filter(subsequence => subsequence !== '');
-}
-
-const commonSubsequence = (s1: string, s2: string): string => {
-  return Array.from(s1).map((char, index) => s2.indexOf(char, index) !== -1 ? char : '').join('');
-}
+const commonSubsequence = (s1: string, s2: string): string =>
+  Array.from(s1).map((char, index) => s2.indexOf(char, index) !== -1 ? char : '').join('');
 
 console.log(longestSubsequence('abazdc', 'bacbad')); // abad
 console.log(longestSubsequence('aggtab', 'gxtxayb')); // gtab
