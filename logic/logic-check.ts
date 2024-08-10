2e1631f9a81a4741e1488917eb9e8397916d2082
Adam Kuziemski <kuziemski.a@gmail.com>
2024-08-10 11:32:02 +0200

    Add printing truth tables and another real life example

--- logic/logic-check.ts
+++ logic/logic-check.ts
@@ -5,15 +5,37 @@ const parameterLength = (callbacks: Function[]): number => Math.max(...callbacks
 const hasIdenticalValues = (comparedValues: any[]): boolean => new Set(comparedValues).size === 1;
 
 const generateTruthTable = (n: number): boolean[][] => Array.from(Array(2 ** n)).map(
-  (_, i) => Array.from(Array(n)).map((_, j) => !!(i & (2 ** j)))
+    (_, i) => Array.from(Array(n)).map((_, j) => !!(i & (2 ** j)))
 );
 
-const forAllPredicates = (predicates: Predicate[], propositionalVariables: boolean[]) => predicates.map(
-  predicate => predicate(...propositionalVariables)
-);
+const printTruthTable = (predicates: Predicate[], propositionalVariables: boolean[], results: boolean[]) => {
+    console.table(predicates.map((p, i) => {
+        const predicateBody = p.toString();
+        const varsStart = predicateBody.indexOf('(');
+        const varsEnd = predicateBody.indexOf(')');
+        const variables = predicateBody.slice(varsStart + 1, varsEnd).split(',').map(v => v.trim());
+        const row = variables.reduce((rowData, v, k) => {
+            rowData[v] = propositionalVariables[k];
+            return rowData;
+        }, {} as { [key: string]: any });
+        row.predicate = predicateBody;
+        row.result = results[i];
+        return row;
+    }));
+};
 
-const isTheSameReasoning = (predicates: Predicate[]): boolean => generateTruthTable(parameterLength(predicates))
-  .every((row: boolean[]) => hasIdenticalValues(forAllPredicates(predicates, row)));
+const forAllPredicates = (predicates: Predicate[], propositionalVariables: boolean[], printTable: boolean) => {
+    const results = predicates.map(predicate => predicate(...propositionalVariables));
+
+    if (printTable) {
+        printTruthTable(predicates, propositionalVariables, results);
+    }
+
+    return results;
+};
+
+const isTheSameReasoning = (predicates: Predicate[], printTable: boolean = false): boolean => generateTruthTable(parameterLength(predicates))
+    .every((row: boolean[]) => hasIdenticalValues(forAllPredicates(predicates, row, printTable)));
 
 // De Morgan's laws
 isTheSameReasoning([
@@ -36,4 +58,9 @@ isTheSameReasoning([
   (p, q, r) => p && q ? r : true,
   (p, q, r) => !(p && q) || r,
   (p, q, r) => !p || !q || r // use De Morgan's law on the one above
+]);
+
+isTheSameReasoning([
+    (p, q, r) => (!p || !q) ? false : !r,
+    (p, q, r) => p && q && !r
 ]);
